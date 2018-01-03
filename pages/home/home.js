48df9a50913d9c40dde570ce05b374fa53d88506
黄伟 <huangwei10@jd.com>
2018-01-03 18:44:23 +0800

    first commit

---
+++ pages/home/home.js
@@ -0,0 +1,33 @@
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    menuList: [
+      {
+        index: 1,
+        name: '母婴喂养',
+        iconUrl: ''
+      },
+      {
+        index: 2,
+        name: '护肤美妆',
+        iconUrl: ''
+      },
+      {
+        index: 3,
+        name: '首饰精品',
+        iconUrl: ''
+      },
+      {
+        index: 4,
+        name: '最新上架',
+        iconUrl: ''
+      }
+    ]
+  },
+  onLoad: function () {
+    this.setData({
+
+    })
+  }
+})
